4b9902779a7025288880a154af5a2a156554f27b
Debiddo <dzheng@terpmail.umd.edu>
2023-09-14 10:54:10 -0400

    Add a column

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with col1:
 with col2:
     st.title("David Zheng")
     content="""
-    Hi, I am David. I am an applied Math graduate. I am interest in math, data, and programming. I am currently taking online classes and working on buliding my profolio.
-    This is a simple webpage showcasing some of the apps that I created using Python as the primary language
+    Hi, I am David. I graduated in 2021 with a Bachelor of Science in Applied Mathematics. I am currently taking online classes and working on building my profolio.
     """
     st.info(content)
+st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
